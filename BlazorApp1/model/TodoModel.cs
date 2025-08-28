2201dd9497d2410e385107ff0896a73e82261d55
wepre <1771555693@qq.com>
2025-08-28 15:36:25 +0800

    feat:通过依赖注入的方式来获取数据 并且新引入了一个onInitialized函数，类似于小程序的onload

--- BlazorApp1/model/TodoModel.cs
+++ BlazorApp1/model/TodoModel.cs
@@ -1,13 +1,15 @@
 namespace BlazorApp1.model;
 
+//TODO 弄成服务来进行注册，依赖注入的形式。
 public class TodoModel
 {
-    private List<ToDo> todoList=[];
+    private List<ToDo> todoList = [];
 
-   public void Add(string title, string content)
+    public void Add(string title, string content)
     {
-        todoList.Add(new ToDo(todoList.Count+1, title, content));
+        todoList.Add(new ToDo(todoList.Count + 1, title, content));
     }
+
     void delete(int id)
     {
         todoList.RemoveAt(id);
@@ -19,5 +21,17 @@ public class TodoModel
         {
             Console.WriteLine($"ID:{toDo.id} title:{toDo.title} content:{toDo.content}");
         }
+    }
+
+    public async Task<List<ToDo>> getAllDataAsync()
+    {
+        // 模拟异步IO操作（如数据库查询/API调用）
+        await Task.Delay(500);
+        var result = new List<ToDo>{
+            new ToDo(1, "标题一", "内容一"),
+            new ToDo(2, "标题二", "内容二"),
+            new ToDo(3, "标题三", "内容三")
+        };
+        return result;
     }
 }
